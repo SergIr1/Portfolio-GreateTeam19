d6f404660ccf193ec333c4f02f2571559370afd4
Serhii Karskyi <serhiipraktic7@gmail.com>
2025-02-05 14:10:56 +0000

    add sections

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+import './portfolio-api.js';
+import './render-functions.js';
+
+import './header.js';
+import './hero';
+import './about-me.js';
+import './benefits.js';
+import './projects.js';
+import './faq.js';
+import './covers.js';
+import './reviews.js';
+import './footer.js';
