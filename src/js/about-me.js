bf901da25050578fc769fc08f6053defb46e60a5
Serhii Karskyi <serhiipraktic7@gmail.com>
2025-02-06 16:02:01 +0000

    acordion

---
+++ src/js/about-me.js
@@ -0,0 +1,21 @@
+import Accordion from "accordion-js";
+
+
+// const accordion = new Accordion(".about-me-accordion-container-list");
+
+// const accardionBtnEl = document.querySelector('.about-me-accordion-container-btn');
+
+// console.log(accardionBtnEl)
+
+// document.addEventListener("DOMContentLoaded", initAccordion);
+
+// function initAccordion() {
+//   const accordion = new Accordion(".about-me-accordion-container-list", {
+//     duration: 400, 
+//     showFirst: true,
+//   });
+
+//   console.log("Accordion initialized:", accordion);
+// }
+
+new Accordion(".accordion-container")
