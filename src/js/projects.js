c4b02d38ba66a306e39ca1f026417691ed09fec8
IgorMedvedenko <noveciti0@gmail.com>
2025-02-09 01:07:52 +0200

    fix2

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,55 +1,53 @@
-// import Swiper from 'swiper/bundle';
+import { Swiper, Navigation, Keyboard } from './module-libs';
 
-// import 'swiper/css/bundle';
+// document.addEventListener('DOMContentLoaded', function () {
+//   const loadElements = document.querySelectorAll('load');
+//   const promises = Array.from(loadElements).map(loadElement => {
+//     const src = loadElement.getAttribute('src');
+//     return fetch(src)
+//       .then(response => response.text())
+//       .then(data => {
+//         loadElement.outerHTML = data;
+//       });
+//   });
 
-document.addEventListener('DOMContentLoaded', function () {
-  const loadElements = document.querySelectorAll('load');
-  const promises = Array.from(loadElements).map(loadElement => {
-    const src = loadElement.getAttribute('src');
-    return fetch(src)
-      .then(response => response.text())
-      .then(data => {
-        loadElement.outerHTML = data;
-      });
-  });
+//   Promise.all(promises)
+//     .then(() => {
+//       const swiper = new Swiper('swiper', {
+//         loop: false,
+//         navigation: {
+//           nextEl: '.ps-swiper-button-next',
+//           prevEl: '.ps-swiper-button-prev',
+//         },
+//         keyboard: {
+//           enabled: true,
+//           onlyInViewport: false,
+//         },
+//         on: {
+//           slideChange: function () {
+//             const prevButton = document.querySelector('.ps-swiper-button-prev');
+//             const nextButton = document.querySelector('.ps-swiper-button-next');
 
-  Promise.all(promises)
-    .then(() => {
-      const swiper = new Swiper('swiper', {
-        loop: false,
-        navigation: {
-          nextEl: '.ps-swiper-button-next',
-          prevEl: '.ps-swiper-button-prev',
-        },
-        keyboard: {
-          enabled: true,
-          onlyInViewport: false,
-        },
-        on: {
-          slideChange: function () {
-            const prevButton = document.querySelector('.ps-swiper-button-prev');
-            const nextButton = document.querySelector('.ps-swiper-button-next');
+//             if (swiper.isBeginning) {
+//               prevButton.classList.add('ps-swiper-button-disabled');
+//               prevButton.setAttribute('disabled', true);
+//             } else {
+//               prevButton.classList.remove('ps-swiper-button-disabled');
+//               prevButton.removeAttribute('disabled');
+//             }
 
-            if (swiper.isBeginning) {
-              prevButton.classList.add('ps-swiper-button-disabled');
-              prevButton.setAttribute('disabled', true);
-            } else {
-              prevButton.classList.remove('ps-swiper-button-disabled');
-              prevButton.removeAttribute('disabled');
-            }
+//             if (swiper.isEnd) {
+//               nextButton.classList.add('ps-swiper-button-disabled');
+//               nextButton.setAttribute('disabled', true);
+//             } else {
+//               nextButton.classList.remove('ps-swiper-button-disabled');
+//               nextButton.removeAttribute('disabled');
+//             }
+//           },
+//         },
+//       });
 
-            if (swiper.isEnd) {
-              nextButton.classList.add('ps-swiper-button-disabled');
-              nextButton.setAttribute('disabled', true);
-            } else {
-              nextButton.classList.remove('ps-swiper-button-disabled');
-              nextButton.removeAttribute('disabled');
-            }
-          },
-        },
-      });
-
-      swiper.emit('slideChange');
-    })
-    .catch(error => console.error('Error loading content:', error));
-});
+//       swiper.emit('slideChange');
+//     })
+//     .catch(error => console.error('Error loading content:', error));
+// });
