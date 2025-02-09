1529445e7a84a66dcf80a81a08f45f7fe33de7e4
IrinaShm00 <irynashmonina@gmail.com>
2025-02-09 13:33:43 +0100

    не моду прибрати кнопку і order the project пропалає при кліку на меню

--- src/js/header.js
+++ src/js/header.js
@@ -2,20 +2,23 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuBtn = document.querySelector(".menu-btn-mobil");
     const menu = document.querySelector(".menu");
     const closeBtn = document.querySelector(".close-btn");
-    const menuItems = document.querySelectorAll(".menu ul li a");
+    const orderBtn = document.querySelector(".order-btn");
 
     // Открытие меню
     menuBtn.addEventListener("click", function (event) {
         event.stopPropagation();
         menu.classList.add("show");
         menu.classList.remove("hide");
-        document.body.style.overflow = "hidden"; // Отключаем скролл страницы
+        orderBtn.style.display = "block"; // Показываем кнопку "Order the project"
+        document.body.style.overflow = "hidden";
+      
     });
 
     // Закрытие меню
     function closeMenu() {
         menu.classList.remove("show");
         menu.classList.add("hide");
+        orderBtn.style.display = "none"; // Скрываем кнопку, когда меню закрыто
         document.body.style.overflow = ""; // Включаем скролл
     }
 
@@ -26,6 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     // Закрытие меню по клику на пункт меню
+    const menuItems = document.querySelectorAll(".menu ul li a");
     menuItems.forEach(item => {
         item.addEventListener("click", function () {
             closeMenu();
